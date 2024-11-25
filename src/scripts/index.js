b78b0ba5aa3912c09d0fbb89692958a3b6bd0dd4
BraulioCompean <22690128@tecvalles.mx>
2024-11-24 23:37:06 -0600

    update : ✨ cambios pagina : principal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,24 @@
 const recomendadosButton = document.getElementById("recommended-page")
 const loginButton = document.getElementById("login-container")
+const modal = document.getElementById("modal")
+const bookCards = document.querySelectorAll(".book-card")
+
+
+
+window.addEventListener("click" ,function(event){
+    if(event.target == modal){
+        modal.style.display = "none"
+    }
+})
+
+
+bookCards.forEach((element)=>{  //AGREGAMOS UN EVENT LISTENER A CADA ELEMENTO QUE TENGA LA CLASE book-card
+    element.addEventListener("click",()=>{
+        openModalWindow() //CADA VEZ QUE SE HAGA CLICK EN UN BOOK CARD SE ABRIRA EL MODAL
+    })
+})
+
+
 
 function openRecommendedBooksPage () {
     window.location = './pages/recommended-books.html'
@@ -18,3 +37,8 @@ function openLoginPage(){
 loginButton.addEventListener("click",()=>{
     openLoginPage()
 })
+
+
+function openModalWindow(){
+    modal.style.display = "flex"
+}
