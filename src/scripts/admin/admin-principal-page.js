fcc40bba2dbe782c050b71083ed46a7dbb495e80
BraulioCompean <22690128@tecvalles.mx>
2024-11-28 07:49:39 -0600

    aasdsdasd

--- src/scripts/admin/admin-principal-page.js
+++ src/scripts/admin/admin-principal-page.js
@@ -1,8 +1,20 @@
 const modal = document.getElementById("modal")
 const añadirBtn = document.getElementById("agregar")
 
-añadirBtn.addEventListener("click",()=>{
+
+function openModalWindow(){
     modal.style.display = "flex"
+}
+function closeModalWindow(){
+    modal.style.display = "none"
+}
+
+
+añadirBtn.addEventListener("click",()=>{
+    openModalWindow()
 })
-
-
+window.addEventListener("click" ,function(event){
+    if(event.target == modal){
+        closeModalWindow()
+    }
+})
