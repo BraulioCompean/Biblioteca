08b56bb4c215948b2229f0ee7ea0a5c5b4fac32f
BraulioCompean <22690128@tecvalles.mx>
2024-12-11 06:08:37 -0600

    asasg

--- src/pages/admin/admin-principal-page.php
+++ src/pages/admin/admin-principal-page.php
@@ -245,7 +245,7 @@
                     <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
                     <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
                 </svg>
-                <!-- <h3 id="header-user-name">
+                <h3 id="header-user-name">
                     <h3 id="header-user-name">
                         <?php
                         require_once '../../db/Database.php';
@@ -271,7 +271,7 @@
                         ?>
 
                     </h3>
-                </h3> -->
+                </h3>
             </a>
         </header>
         <nav class="opciones-navegar-principal">
