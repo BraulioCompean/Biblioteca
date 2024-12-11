497b1d2349fad3e20dbb9fee1c2fde80c73f8582
BraulioCompean <22690128@tecvalles.mx>
2024-12-10 22:32:24 -0600

    asdsad

--- src/db/Database.php
+++ src/db/Database.php
@@ -8,7 +8,7 @@ class Database {
 
     private function connect() {
         // $dsn = "pgsql:host=ep-quiet-frost-a5om3mkk.us-east-2.aws.neon.tech;port=5432;dbname=neondb;sslmode=require";
-        $dsn = "postgresql://neondb_owner:eHsJkUPn41Wm@ep-quiet-frost-a5om3mkk.us-east-2.aws.neon.tech/neondb?sslmode=require&options=endpoint%3Dep-quiet-frost-a5om3mkk";
+        $dsn = "pgsql:host=ep-quiet-frost-a5om3mkk.us-east-2.aws.neon.tech;port=5432;dbname=neondb;sslmode=require&options=endpoint%3Dep-quiet-frost-a5om3mkk";
         $user = "neondb_owner";
         $password = "eHsJkUPn41Wm";
 
